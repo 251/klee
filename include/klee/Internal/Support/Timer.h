58958eac20a33ce26af908c5eee02ea254578b85
Frank Busse <bb0xfb@gmail.com>
2019-07-30 18:45:42 +0100

    ExecutorTimers: refactor and move to support lib
    - moves timer handling from Executor into support lib
    - introduces TimerGroup, removes TimerInfo class
    - removes ExecutorTimers.cpp and ExecutorTimerInfo.h
    - removes -max-instruction-time flag (see #1114)

--- include/klee/Internal/Support/Timer.h
+++ include/klee/Internal/Support/Timer.h
@@ -12,16 +12,66 @@
 
 #include "klee/Internal/System/Time.h"
 
+#include "llvm/ADT/SmallVector.h"
+
+#include <functional>
+#include <memory>
+
 namespace klee {
+
   class WallTimer {
     time::Point start;
-    
+
   public:
     WallTimer();
 
     /// check - Return the delta since the timer was created
     time::Span check();
   };
-}
+
+
+  class Timer {
+    /// Approximate delay per timer firing.
+    time::Span rate;
+    /// Wall time for next firing.
+    time::Point nextFireTime;
+  public:
+    /// The event callback.
+    std::function<void()> run;
+
+    /// \param rate The time span between firings.
+    /// \param callback The event callback.
+    Timer(const time::Span &rate, std::function<void()> &&callback);
+    ~Timer() = default;
+
+    /// Return rate.
+    time::Span getRate() const;
+    /// Invoke callback if time for next firing exceeded.
+    void invoke(const time::Point &currentTime);
+    /// Reset timer from current time.
+    void reset(const time::Point &currentTime);
+  };
+
+
+  class TimerGroup {
+    /// Registered timers.
+    llvm::SmallVector<std::unique_ptr<Timer>, 4> timers;
+    /// Minimum interval between timer invocations.
+    const time::Span minInterval;
+    /// Wall time of last timer invocation.
+    time::Point lastCheckedTime;
+  public:
+    explicit TimerGroup(const time::Span &minInterval);
+
+    /// Add a timer to be executed periodically.
+    ///
+    /// \param timer The timer object to run on firings.
+    void add(std::unique_ptr<Timer> timer);
+    /// Invoke timers.
+    void invoke();
+    /// Reset all timers.
+    void reset();
+  };
+} // klee
 
 #endif /* KLEE_TIMER_H */
--- lib/Support/Timer.cpp
+++ lib/Support/Timer.cpp
@@ -7,16 +7,78 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "klee/Config/Version.h"
+#include "klee/Internal/Support/ErrorHandling.h"
 #include "klee/Internal/Support/Timer.h"
 #include "klee/Internal/System/Time.h"
 
+
 using namespace klee;
+
+
+// WallTimer
 
 WallTimer::WallTimer() {
   start = time::getWallTime();
 }
 
 time::Span WallTimer::check() {
-  return time::Span(time::getWallTime() - start);
+  return {time::getWallTime() - start};
+}
+
+
+// Timer
+
+Timer::Timer(const time::Span &rate, std::function<void()> &&callback) :
+  rate{rate}, nextFireTime{time::getWallTime() + rate}, run{std::move(callback)} {};
+
+time::Span Timer::getRate() const {
+  return rate;
+};
+
+void Timer::invoke(const time::Point &currentTime) {
+  if (currentTime < nextFireTime) return;
+
+  run();
+  nextFireTime = currentTime + rate;
+};
+
+void Timer::reset(const time::Point &currentTime) {
+  nextFireTime = currentTime + rate;
+};
+
+
+// TimerGroup
+
+TimerGroup::TimerGroup(const time::Span &minInterval) : minInterval{minInterval}, lastCheckedTime{time::getWallTime()} {};
+
+void TimerGroup::add(std::unique_ptr<klee::Timer> timer) {
+  const auto &rate = timer->getRate();
+  if (rate < minInterval)
+    klee_warning("Timer rate below minimum timer interval (-timer-interval)");
+  if (rate.toMicroseconds() % minInterval.toMicroseconds())
+    klee_warning("Timer rate not a multiple of timer interval (-timer-interval)");
+
+  timers.emplace_back(std::move(timer));
+}
+
+void TimerGroup::invoke() {
+  const auto currentTime = time::getWallTime();
+  const auto currentInterval = currentTime - lastCheckedTime;
+
+  // short circuit if below TimerInterval
+  if (currentInterval < minInterval)
+    return;
+
+  // invoke timers
+  for (auto &timer : timers)
+    timer->invoke(currentTime);
+
+  lastCheckedTime = currentTime;
+}
+
+void TimerGroup::reset() {
+  const auto currentTime = time::getWallTime();
+  for (auto &timer : timers)
+    timer->reset(currentTime);
+  lastCheckedTime = currentTime;
 }
